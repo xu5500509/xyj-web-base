3c87131f87e39e41220ea016e5833c413a8707e0
xuyangjian <397008849@qq.com>
2018-01-11 14:26:09 +0800

    增加定时任务例子：测试出同一个定时任务不是并发执行的

---
+++ src/main/java/cn/xyj/ssm/task/MyProcessor.java
@@ -0,0 +1,54 @@
+package cn.xyj.ssm.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 测试定时任务
+ * Created by xuyangjian on 2018/1/11.
+ */
+@Component
+public class MyProcessor{
+
+    DateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+    int[] delays = new int[]{8,3,6,2,2};
+    int index = 0;
+    int process2Index = 0;
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void process() {
+        try {
+            if(index > delays.length - 1){
+                if(index == delays.length){
+                    System.out.println("---------- test end at " + sdf.format(new Date()) + " ---------");
+                }
+                index ++;
+                return;
+            }else{
+                System.out.println(index + ":process start run at" + sdf.format(new Date()));
+            }
+            Thread.sleep(delays[index] * 1000);
+            System.out.println(index + ":process end run at " + sdf.format(new Date()));
+            index ++;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "0/1 * * * * ?")
+    public void process2() {
+        try {
+            System.out.println(process2Index + ":process2 start run at" + sdf.format(new Date()));
+            Thread.sleep(5000);
+            System.out.println(process2Index + ":process2 end run at " + sdf.format(new Date()));
+            process2Index ++;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
